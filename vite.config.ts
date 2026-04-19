5fd8bcf30ddde976642a54e3f2f329cada9c2752
jahaumeps <40784527+jahaumeps@users.noreply.github.com>
2026-04-19 20:08:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import {defineConfig, loadEnv} from 'vite';
 
 
 export default defineConfig(({mode}) => {
-  base: '/equipment/', 
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/equipment/', 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
